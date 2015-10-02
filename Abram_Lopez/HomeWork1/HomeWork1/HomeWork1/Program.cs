29fe3dec648676f7b549039c553a130cab5b5082
awlopez <abramwelo@gmail.com>
2015-10-02 08:20:15 -0700

    HomeWork1 Complete

--- Abram_Lopez/HomeWork1/HomeWork1/HomeWork1/Program.cs
+++ Abram_Lopez/HomeWork1/HomeWork1/HomeWork1/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿//"using" calls built in libraries built in with Visual Studio 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWork1
+namespace FirstProject //treat as a last name/family name of a person
 {
-    class Program
+    class Program //treat as the first name of a person, classes can have multiple first names because you are all part of the same family
     {
-        static void Main(string[] args)
+        static void Main() //This is the entry point for the application
         {
+            Console.Write("Hello World!"); //C# lines are always closed with a ;
+            Console.ReadLine();
         }
     }
 }
